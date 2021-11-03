4c2139adac9066a02729f815f05d8132e9344afe
William Boudreault <boudreaultwilliam@gmail.com>
2021-11-03 17:37:47 -0400

    Ajout du saut du personnage

--- scripts/Joueur.gd
+++ scripts/Joueur.gd
@@ -2,18 +2,22 @@ extends KinematicBody2D
 
 # Variables #
 var velocity = Vector2.ZERO
+var gravite = Vector2.ZERO
+export (int) var vitesse_saut = -900
 export (int) var vitesse_max = 250
 export (float) var deceleration = 0.88
 export (int) var acceleration = 50
 var accroupi = false
+var saute = false
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	recevoir_input()
-	se_deplacer()
 	changer_animation()
+	calc_gravite()
+	se_deplacer()
 
 func recevoir_input():
 	if (Input.is_action_pressed("ui_down")):
@@ -21,6 +25,9 @@ func recevoir_input():
 
 	if (Input.is_action_just_released("ui_down")):
 		accroupi = false
+	
+	if (Input.is_action_just_pressed("ui_up") && $Gravity.is_colliding()):
+		saute = true
 
 	if (Input.is_action_pressed("ui_right")):
 		$Sprite_joueur.flip_h = false
@@ -31,16 +38,26 @@ func recevoir_input():
 		if !accroupi:
 			velocity.x -= acceleration
 
+func calc_gravite():
+	if ($Gravity.is_colliding()):
+		gravite.y = 0
+		if saute:
+			gravite.y = vitesse_saut
+			saute = false
+	else:
+		gravite.y += acceleration
+
 func se_deplacer():
-	if (velocity != Vector2.ZERO):
+	if (velocity != Vector2.ZERO || gravite != Vector2.ZERO):
 		velocity.clamped(vitesse_max)
-		move_and_slide(velocity)
+		gravite.clamped(vitesse_max)
+		move_and_slide(velocity + gravite)
 		velocity *= deceleration
 		if (velocity.length() <= 15):
 			velocity = Vector2.ZERO
 
 func changer_animation():
-	if (velocity.y <= 15):
+	if (gravite.y == 0):
 		if accroupi:
 			if ($Sprite_joueur.animation != "crouch"):
 				$Sprite_joueur.animation = "crouch"
@@ -48,3 +65,6 @@ func changer_animation():
 			$Sprite_joueur.animation = "idle"
 		else:
 			$Sprite_joueur.animation = "running"
+			
+	if (!$Gravity.is_colliding()):
+		$Sprite_joueur.animation = "jump"
